e9bb73a5b2eecf42c124cf7c4b854d5e31f6021e
vaibhavi306 <pawarvaibhavi030@gmail.com>
2025-06-27 10:14:25 +0530

    Update and rename app.py to index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// This is a basic PHP script
+
+echo "Hello, World!<br>";
+
+date_default_timezone_set("Asia/Kolkata"); // Set your timezone
+echo "Current date and time is: " . date("Y-m-d H:i:s");
+?>
